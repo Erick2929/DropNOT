b6ff5e3ece1b6ab205abcba12fbf24f4878f30d8
Jorge Becerril <110145796+jorgebecerrilgi@users.noreply.github.com>
2022-09-24 22:22:00 -0500

    Finished HomeScreen.js mockup clone. Added more pseudo bootstrap classes (col2,10,3,6). Added alert class, and placeholder alert symbol.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,29 +1,75 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, View, Text, Image } from 'react-native';
 
 const HomeScreen = () => {
     return (
-        <View style={style.textCenter}>
-            <Text style={style.title}>Consumo de Hoy</Text>
-            <Image style={style.img} source={require('../../assets/placeholder-water.png')}></Image>
-            <Text style={style.title}>Consumo General</Text>
-            <View style={style.card}>
-               <Text style={style.subtitle}>Su consumo bajó <Text style={style.highlighted}>2%</Text></Text>
-               <Text>¡Usaste <Text style={style.highlighted}>19.04 litros</Text> menos que la semana pasada!</Text>
-            </View>
-            <View style={style.card}>
-                <View style={style.row}>
-                    <View style={[style.col6, style.textCenter]}>
-                        <Text style={style.subtitle}>Semana Pasada</Text>
-                        <Text style={style.title}>952 L</Text>
+        <SafeAreaView>
+            <ScrollView>
+                <View style={[style.textCenter, {paddingTop: 10}]}>
+                    <Text style={style.title}>Consumo de Hoy</Text>
+                    <Image style={style.img} source={require('../../assets/placeholder-water.png')}></Image>
+                    {/* Alerta BEGIN */}
+                    <View style={style.alert}>
+                        <View style={style.row}>
+                            <View style={style.col2}>
+                                <Image 
+                                    source={require('../../assets/placeholder-warning.png')}
+                                    style={{
+                                        resizeMode: 'contain',
+                                        width: '50%',
+
+                                        alignSelf: 'flex-end',
+                                        marginHorizontal: 10,
+                                    }}
+                                ></Image>
+                            </View>
+                            <View style={style.col9}>
+                                <Text>
+                                    Alerta: Se ha detectado que el nut 
+                                    <Text style={style.highlighted}> Lavabo Cocina </Text> 
+                                    ha permanecido abierto por un periodo prolongado.
+                                </Text>
+                            </View>
+                        </View>
                     </View>
-                    <View style={[style.col6, style.textCenter]}>
-                        <Text style={[style.subtitle, style.correct]}>Semana Actual</Text>
-                        <Text style={style.title}>952 L</Text>
+                    {/* Alerta END */}
+                    <Text style={style.title}>Consumo General</Text>
+                    {/* Consumo BEGIN */}
+                    <View style={style.card}>
+                        <Text style={style.subtitle}>Su consumo bajó <Text style={style.highlighted}>14.56%</Text></Text>
+                        <Text>¡Usaste <Text style={style.highlighted}>159.04 litros</Text> menos que la semana pasada!</Text>
                     </View>
+                    {/* Consumo END */}
+                    {/* Semanal BEGIN */}
+                    <View style={style.card}>
+                        <View style={style.row}>
+                            <View style={[style.col6, style.textCenter]}>
+                                <Text style={style.subtitle}>Semana Pasada</Text>
+                                <Text style={style.title}>1092 L</Text>
+                            </View>
+                            <View style={[style.col6, style.textCenter]}>
+                                <Text style={[style.subtitle, style.correct]}>Semana Actual</Text>
+                                <Text style={style.title}>932.96 L</Text>
+                            </View>
+                        </View>
+                    </View>
+                    {/* Semanal END */}
+                    {/* Información Relevante BEGIN */}
+                    <Text style={style.title}>Información Relevante</Text>
+                    <View style={style.card}>
+                        <Text style={style.title}>136 Litros</Text>
+                        <Text>
+                            El consumo de agua en los hogares varía enormemente 
+                            según el número de personas que hayan en casa, y sus 
+                            necesidades personales. Una persona consume una 
+                            media de 136 litros por día. Todos los días se 
+                            pierden 112 litros por propiedad, por fugas.
+                        </Text>
+                    </View>
+                    {/* Información Relevante END */}
                 </View>
-            </View>
-        </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
@@ -54,7 +100,11 @@ const style = StyleSheet.create({
         alignItems: 'center'
     },
     img: {
-        borderRadius: 50
+        borderRadius: 50,
+    },
+    alert: {
+        width: '90%',
+        padding: 20,
     },
     card: {
         backgroundColor: 'white',
@@ -66,8 +116,20 @@ const style = StyleSheet.create({
     row: {
         flexDirection: 'row'
     },
+    col10: {
+        flex: 0.835
+    },
+    col9: {
+        flex: 0.75
+    },
     col6: {
         flex: 0.5
+    },
+    col3: {
+        flex: 0.25
+    },
+    col2: {
+        flex: 0.165
     }
 });
 
